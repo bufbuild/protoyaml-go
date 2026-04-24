ad20a549bbc02afe2abf0d13e32e3e11d4c7656d
Stefan VanBuren <svanburen@buf.build>
2026-04-24 16:04:46 -0400

    Upgrade repository (#71)
    Similar to bufbuild/standard-go#6.

--- decode.go
+++ decode.go
@@ -1,4 +1,4 @@
-// Copyright 2023-2024 Buf Technologies, Inc.
+// Copyright 2023-2026 Buf Technologies, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -222,7 +222,7 @@ func (u *unmarshaler) addError(node *yaml.Node, err error) {
 		line:  u.lines[node.Line-1],
 	})
 }
-func (u *unmarshaler) addErrorf(node *yaml.Node, format string, args ...interface{}) {
+func (u *unmarshaler) addErrorf(node *yaml.Node, format string, args ...any) {
 	u.addError(node, fmt.Errorf(format, args...))
 }
 
